14f4ac5fa53801c7457105ff3f1ce83d27c3ecbd
William Lightning <wlightning@fuelmedical.com>
2025-11-18 10:18:56 -0800

    Create devcontainer so that it's the host instead of inside the container

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo --- Copy the environment file ...
+cp .env.example .env
+
+echo "--- Install dependencies (first sail install will take a while) ..."
+composer install
+
+echo "--- Start sail containers ..."
+./vendor/bin/sail up -d
+
+echo "--- Generate the application key ..."
+./vendor/bin/sail php artisan key:generate
+
+echo "--- Install npm packages ..."
+./vendor/bin/sail npm install
+
+echo "--- Stop sail containers ..."
+./vendor/bin/sail down -v
+
+echo "--- SETUP DONE ---"
+echo "--- Please read README.md to start reproducing the github issue ---"
